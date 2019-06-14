888f6a1d108c3b0a9781c88ae870e88143e9b657
somePerson2xxx <personxxxsmith@gmail.com>
2019-06-14 13:09:19 +0300

    print hey students string

--- com/src/Main.java
+++ com/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String...args){
         System.out.println("hello world");
         System.out.println("another test");
-        System.out.println(90+80);
+        System.out.println("hey students");
     }
 }
