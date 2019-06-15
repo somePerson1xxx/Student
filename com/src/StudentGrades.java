ee14824ecf2387d544a1101355cc8b96e7616f01
somePerson2xxx <personxxxsmith@gmail.com>
2019-06-15 19:03:11 +0300

    add english to grades and initlize it

--- com/src/StudentGrades.java
+++ com/src/StudentGrades.java
@@ -3,11 +3,12 @@
    private int math;
    private int phys;
    private int chem;
-
+   private int eng;
    public StudentGrades(){}
-   public StudentGrades(int math,int phys,int chem){
+   public StudentGrades(int math,int phys,int chem,int eng){
        this.math=math;
        this.phys=phys;
        this.chem=chem;
+       this.eng=eng;
    }
  }
